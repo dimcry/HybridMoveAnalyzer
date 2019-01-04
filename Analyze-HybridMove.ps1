b5b242414ee0beaac3a2fbf3fe2cb7287110aeb1
Cristian Dimofte <cristid@microsoft.com>
2019-01-04 17:15:39 +0200

    Commit

--- Analyze-HybridMove.ps1
+++ Analyze-HybridMove.ps1
@@ -66,6 +66,22 @@ The logic:
 # Common space for functions, global variables #
 ################################################
 
+function Provide-PathOfXMLFile {
+
+    Write-Host
+    Write-Host "Please provide the exact path of the .xml file: " -ForegroundColor  Cyan
+    Write-Host "`t" -NoNewline
+    ##### Validation needed (the path)
+    [string]$PathOfTheFile = Read-Host
+
+    [bool]$ValidationOfThePath = Check-PathOrFile -PathToCheck $PathOfTheFile
+
+    if ($ValidationOfThePath) {
+        return $PathOfTheFile
+    }
+
+}
+
 function Check-PathOrFile {
     param (
         # Full path of the .xml file
@@ -74,12 +90,17 @@ function Check-PathOrFile {
         $PathToCheck
     )
 
+    Write-Host
+    Write-Host "We are validating the path you provided..." -ForegroundColor Cyan
     [bool]$ValidatePath = $True
     if (!(Test-Path "$PathToCheck" -ErrorAction SilentlyContinue))
     {
         $ValidatePath = $False
         Write-Host "The following is an invalid path, or, we are not able to connect to it: " -ForegroundColor Red -NoNewline
         Write-Host " `"$PathToCheck`" " -ForegroundColor White -NoNewline
+    }
+    else {
+        Write-Host "`tPath successfully validated..." -ForegroundColor Green
     }
     
     return $ValidatePath
@@ -93,21 +114,140 @@ function Import-XMLInVariable {
         $FileToImport
     )
 
-    $TheFile = Import-Clixml $FileToImport
+    Write-Host
+    Write-Host "We are importing the file into a PowerShell variable..." -ForegroundColor Cyan
+    [PSObject]$TheFile = Import-Clixml $FileToImport
 
-    return $TheFile
+    if ($TheFile) {
+        Write-Host "`tWe successfully imported the file..." -ForegroundColor Green
+        return $TheFile
+    }
+    else {
+        Import-XMLInVariable -FileToImport $FileToImport
+    }
 }
 
+function Provide-AffectedUser {
+
+    Write-Host
+    Write-Host "Please provide the email address or UserPrincipalName of the affected user: " -ForegroundColor Cyan
+    Write-Host "`t" -NoNewline
+    ##### Validation needed (email address)
+    [string]$AffectedUser = Read-Host
+
+    return $AffectedUser
+
+}
+
+function Validate-ConnectionToExchangeOnline {
+
+    Write-Host "Checking if you are connected to Exchange Online..."
+    ##### Validation needed (PSSession)
+    [bool]$AlreadyConnectedToEXO = $true
+    $GetPsSessions = Get-PSSession -ErrorAction SilentlyContinue
+
+    foreach ($PsSession in $GetPsSessions){
+        if (($($PsSession.ComputerName).ToLower() -eq "outlook.office365.com") -and ($($PsSession.State).ToString().ToLower() -eq "opened")) {
+
+        }
+
+    }
+    
+    
+}
+
+function Connect-ToExchangeOnline {
+
+    Write-Host
+    Write-Host "We are connecting you to Exchange Online..." -ForegroundColor Cyan
+    Write-Host
+    $cred = Get-Credential
+    $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $cred -Authentication Basic -AllowRedirection
+
+    return $Session
+    
+}
+
+function Validate-MoveRequestStatistics {
+    param (
+        # .xml file to validate
+        [Parameter(Mandatory = $true)]
+        [PSObject]
+        $MoveRequestStatisticsToValidate
+    )
+ 
+    Write-Host
+    Write-Host "We are validating if the information from the file you provided can be used for further analysis..." -ForegroundColor Cyan
+    [bool]$InfoValidated = $false
+
+    [xml]$DiagnosticInfoToCheck = $($MoveRequestStatisticsToValidate.DiagnosticInfo)
+    if (($($MoveRequestStatisticsToValidate.Report)) -and ($($DiagnosticInfoToCheck.Job.TimeTracker.Durations)) -and ($($DiagnosticInfoToCheck.Job.TimeTracker.Timeline))) {
+        [bool]$InfoValidated = $true
+        Write-Host "`tThe information from the file will be used for further analysis..." -ForegroundColor Green
+    }
+    else {
+        Write-Host "`tThe information from the file will not be used for further analysis. We will use other methods to collect the information related to the move..." -ForegroundColor Red
+    }
+
+    return $InfoValidated
+}
 
 
 ###############
 # Main script #
 ###############
 
+Get-PSSession | Remove-PSSession
+
 Write-Host "Do you have the output of the " -ForegroundColor White -NoNewline
 Write-Host "Get-MoveRequestStatistics <AffectedMailbox> -IncludeReport -DiagnosticInfo `"showtimeslots, showtimeline, verbose`"" -ForegroundColor Cyan -NoNewline
 Write-Host " command in an .xml file (Y / N)? " -ForegroundColor White -NoNewline
-$ReadFromKeyboard = ""
+##### Validation needed (y/n)
+[char]$ReadFromKeyboard = Read-Host
+
+[bool]$ValidationOfKey = $false
+if ($ReadFromKeyboard.ToString().ToUpper() -eq "Y") {
+    [string]$ThePathOfTheFile = Provide-PathOfXMLFile
+    [PSObject]$TheMoveRequestStatistics = Import-XMLInVariable -FileToImport $ThePathOfTheFile
+    
+    [bool]$IsValidToBeUsed = Validate-MoveRequestStatistics -MoveRequestStatisticsToValidate $TheMoveRequestStatistics
+
+    if (-not ($IsValidToBeUsed)) {
+        $BackupMoveRequestStatistics = $TheMoveRequestStatistics
+        $TheMoveRequestStatistics = New-Object PSObject
+    }
+    else {
+        $ValidationOfKey = $true
+    }
+
+}
+if (-not ($ValidationOfKey)) {
+    [string]$EmailAddress = Provide-AffectedUser
+    $EXOPSSession = Connect-ToExchangeOnline
+    $ImportedSession = Import-PSSession $EXOPSSession -Prefix "EXO" -AllowClobber -ErrorAction SilentlyContinue
+
+    Write-Host
+    Write-Host "We are collecting the output of " -ForegroundColor Cyan -NoNewline
+    Write-Host "`"Get-EXOMoveRequestStatistics $EmailAddress -IncludeReport -DiagnosticInfo `"showtimeslots, showtimeline, verbose`"" -ForegroundColor White -NoNewline
+    Write-Host " from Exchange Online..." -ForegroundColor Cyan
+    
+    ##### Validation needed (Connected to EXO? Rights to run the command?)
+
+    $TheMoveRequestStatistics = Get-EXOMoveRequestStatistics $EmailAddress -IncludeReport -DiagnosticInfo "showtimeslots, showtimeline, verbose" -ErrorAction SilentlyContinue
+
+
+    <#if (-not ($TheMoveRequestStatistics)) {
+        # Are we still connected to Exchange Online?
+        Get-Command Get-EXOMoveRequestStatistics
+    }#>
+}
+
+Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope Process -Confirm:$false
+# Unblock-File .\AnalyzeMoveRequestStats.ps1
+. .\AnalyzeMoveRequestStats.ps1
+
+$stats = $TheMoveRequestStatistics
+ProcessStats -stats $stats -name ProcessedStats1
 
 
 #$MoveRequestStatistics = Import-Clixml C:\Temp\Doinita\dtest2.xml
@@ -119,3 +259,36 @@ $ReadFromKeyboard = ""
 
 ############################
 
+
+############################
+#####################################
+# Create / update .xml / .json file #
+#####################################
+<#
+### If .xml, try not to use Import/Export-Clixml
+
+[System.Collections.ArrayList]$ErrorsAndRecommendations = @()
+
+$TheXML = New-Object -TypeName PSObject -ErrorAction SilentlyContinue
+$TheXML | Add-Member -Type NoteProperty -Name "Failure Type" -Value "NoValue"
+$TheXML | Add-Member -Type NoteProperty -Name "Actual Issue" -Value "NoValue"
+$TheXML | Add-Member -Type NoteProperty -Name "Recommendation" -Value "NoValue"
+$void = $ErrorsAndRecommendations.Add($TheXML)
+
+$TheXML1 = New-Object -TypeName PSObject -ErrorAction SilentlyContinue
+$TheXML1 | Add-Member -Type NoteProperty -Name "Failure Type" -Value "NoValue"
+$TheXML1 | Add-Member -Type NoteProperty -Name "Actual Issue" -Value "NoValue"
+$TheXML1 | Add-Member -Type NoteProperty -Name "Recommendation" -Value "NoValue"
+$void = $ErrorsAndRecommendations.Add($TheXML1)
+
+$TheXML2 = New-Object -TypeName PSObject -ErrorAction SilentlyContinue
+$TheXML2 | Add-Member -Type NoteProperty -Name "Failure Type" -Value "NoValue"
+$TheXML2 | Add-Member -Type NoteProperty -Name "Actual Issue" -Value "NoValue"
+$TheXML2 | Add-Member -Type NoteProperty -Name "Recommendation" -Value "NoValue"
+$void = $ErrorsAndRecommendations.Add($TheXML2)
+
+$ErrorsAndRecommendations | Export-Clixml .\ErrorsAndRecommendations1.xml -Force
+$JSon_ErrorsAndRecommendations = $ErrorsAndRecommendations | ConvertTo-Json -Depth 10
+$JSon_ErrorsAndRecommendations | Out-File .\JSon_ErrorsAndRecommendations.json -Force
+#>
+############################
