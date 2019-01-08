b1b9eafcf381973a5283fe6034a94c9484725303
Cristian Dimofte <cristid@microsoft.com>
2019-01-08 18:54:43 +0200

    Commit

--- Analyze-HybridMove.ps1
+++ Analyze-HybridMove.ps1
@@ -192,6 +192,226 @@ function Validate-MoveRequestStatistics {
     return $InfoValidated
 }
 
+Function Out-PieChart {
+    [CmdletBinding()]
+    param (
+        [Parameter(ValueFromPipeline)]
+        [psobject] $inputObject,
+        [Parameter()]
+        [string] $PieChartTitle,
+        [Parameter()]
+        [int] $ChartWidth = 800,
+        [Parameter()]
+        [int] $ChartHeight = 400,
+        [Parameter()]
+        [string[]] $NameProperty,
+        [Parameter()]
+        [string] $ValueProperty,
+        [Parameter()]
+        [switch] $Pie3D,
+        [Parameter()]
+        [switch] $DisplayToScreen,
+        [Parameter()]
+        [string] $saveImage
+    )
+    begin {
+        Add-Type -AssemblyName System.Windows.Forms.DataVisualization
+        # Frame
+        $Chart = [System.Windows.Forms.DataVisualization.Charting.Chart]@{
+            Width       = $ChartWidth
+            Height      = $ChartHeight
+            BackColor   = 'White'
+            BorderColor = 'Black'
+        }
+        # Body
+        $null = $Chart.Titles.Add($PieChartTitle)
+        $Chart.Titles[0].Font = "segoeuilight,20pt"
+        $Chart.Titles[0].Alignment = "TopCenter"
+        # Create Chart Area
+        $ChartArea = [System.Windows.Forms.DataVisualization.Charting.ChartArea]::new()
+        $ChartArea.Area3DStyle.Enable3D = $Pie3D.ToBool()
+        $ChartArea.Area3DStyle.Inclination = 50
+        $Chart.ChartAreas.Add($ChartArea)
+        # Define Chart Area
+        $null = $Chart.Series.Add("Data")
+        $Chart.Series["Data"].ChartType = [System.Windows.Forms.DataVisualization.Charting.SeriesChartType]::Pie
+        # Chart style
+        $Chart.Series["Data"]["PieLabelStyle"] = "Outside"
+        $Chart.Series["Data"]["PieLineColor"] = "Black"
+        $Chart.Series["Data"]["PieDrawingStyle"] = "Concave"
+
+        $chart.Series["Data"].IsValueShownAsLabel = $true
+        $chart.series["Data"].Label = "#PERCENT\n#VALX"
+        # Set ArrayList
+        $XColumn = [System.Collections.ArrayList]::new()
+        $yColumn = [System.Collections.ArrayList]::new()
+    }
+    process {
+        if (-not $valueProperty) {
+            $numericProperties = foreach ($property in $inputObject.PSObject.Properties) {
+                if ([Double]::TryParse($property.Value, [Ref]$null)) {
+                    $property.Name
+                }
+            }
+            if (@($numericProperties).Count -eq 1) {
+                $valueProperty = $numericProperties
+            }
+            else {
+                throw 'Unable to automatically determine properties to graph'
+            }
+        }
+        if (-not $LabelProperty) {
+            if ($inputObject.PSObject.Properties.Count -eq 2) {
+                $LabelProperty = $inputObject.Properties.Name -ne $valueProperty
+            }
+            elseif ($inputObject.PSObject.Properties.Item('Name')) {
+                $LabelProperty = 'Name'
+            }
+            else {
+                throw 'Cannot convert Data'
+            }
+        }
+        # Bind chart columns
+        $null = $yColumn.Add($InputObject.$valueProperty)
+        $null = $xColumn.Add($inputObject.$LabelProperty)
+    }
+    end {
+        # Add data to chart
+        $Chart.Series["Data"].Points.DataBindXY($xColumn, $yColumn)
+        # Save file
+        if ($psboundparameters.ContainsKey('saveImage')) {
+            try{
+                if (Test-Path (Split-Path $saveImage -Parent)) {
+                    $SaveImage = $pscmdlet.GetUnresolvedProviderPathFromPSPath($saveImage)
+                    $Chart.SaveImage($saveImage, "png")
+                } else {
+                    throw 'Invalid path, the parent directory must exist'
+                }
+            } catch {
+                throw
+            }
+        }
+        # Display Chart to screen
+        if ($DisplayToScreen.ToBool()) {
+            $Form = [Windows.Forms.Form]@{
+                Width           = 800
+                Height          = 450
+                AutoSize        = $true
+                FormBorderStyle = "FixedDialog"
+                MaximizeBox     = $false
+                MinimizeBox     = $false
+                KeyPreview      = $true
+            }
+            $Form.controls.add($Chart)
+            $Chart.Anchor = 'Bottom, Right, Top, Left'
+            $Form.Add_KeyDown({
+                if ($_.KeyCode -eq "Escape") { $Form.Close() }
+            })
+            $Form.Add_Shown( {$Form.Activate()})
+            $Form.ShowDialog() | Out-Null
+        }
+    }
+}
+
+function Analyze-DiagnosticInfo {
+    param (
+        # XML of DiagnosticInfo to analyze
+        [Parameter(Mandatory = $true)]
+        [PSObject]
+        $DiagnosticInfo
+    )
+    
+
+
+}
+
+function Build-TimeTrackerTable
+{
+    <#
+        .Synopsis
+        Retrieves the set of MRS indexes in AD for jobs matching the given query.
+
+        .Parameter MrsJob
+        An object returned by Get-*RequestStatistics with detailed time-tracker data.
+		These data are obtained by passing these arguments to the cmdlet: -Diagnostic -DiagnosticArgument "showtimeline,verbose"
+
+        .Parameter Aggregation
+        Build the table from the ByMinute, ByHour, ByDay or ByMonth XML aggregations.
+    #>
+    param(
+        [Parameter(Mandatory=$true)]
+        $MrsJob,
+        [Parameter(Mandatory=$false)]
+        [ValidateSet('Minute', 'Hour', 'Day', 'Month')]
+        [string]
+        $Aggregation = 'Hour'
+    )
+
+    $diagnosticInfo = [xml]$MrsJob.DiagnosticInfo
+    if ($diagnosticInfo -eq $null)
+    {
+        return
+    }
+
+	$seriesName = 'By{0}' -f $Aggregation
+    $seriesData = $diagnosticInfo.Job.TimeTracker.Timeline.$seriesName
+    if ($seriesData -eq $null -or $seriesData.$Aggregation.Count -eq 0)
+    {
+        return
+    }
+
+    $seriesSize = $seriesData.$Aggregation.D.Count
+    $series = [System.Collections.Generic.List[object]]::new($seriesSize)
+    foreach ($hour in $seriesData.$Aggregation)
+    {
+        $startTime = $hour.StartTime -as [DateTime]
+        foreach ($entry in $hour.D)
+        {
+            $state = $entry.State
+            $duration = $entry.Duration -as [TimeSpan]
+			$msecs = $entry.MSecs -as [long]
+            $row = [PSCustomObject][Ordered]@{
+                'StartTime' = $startTime
+                'State' = $state
+                'Duration' = $duration
+				'Milliseconds' = $msecs
+				'CumulativeDuration' = $null
+				'CumulativeMilliseconds' = $null
+            }
+
+            $series.Add($row)
+        }
+    }
+
+	$series = $series | sort StartTime, State
+
+	$gSeries = $series | group -NoElement State
+	$accumulations = @{}
+	$gSeries.Name | %{ $accumulations[$_] = [TimeSpan]::Zero }
+
+    $series | %{
+		$state = $_.State
+		$accumulation = $accumulations[$state]
+		$accumulation += $_.Duration
+		$accumulations[$state] = $accumulation
+		$_.CumulativeDuration = $accumulation
+		$_.CumulativeMilliseconds = $accumulation.TotalMilliseconds
+	}
+
+	$series
+}
+
+<#
+function DownloadTheJSonFile {
+
+    $WebClient = New-Object System.Net.WebClient
+    $WebClient.DownloadFile("https://github.com/dimcry/HybridMoveAnalyzer/blob/master/JSon_ErrorsAndRecommendations.json",".\ErrorsAndRecommendations.json")
+  
+}
+
+$Request = "https://github.com/dimcry/HybridMoveAnalyzer/blob/master/JSon_ErrorsAndRecommendations.json"
+Invoke-WebRequest $Request
+#>
 
 ###############
 # Main script #
@@ -249,6 +469,27 @@ Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope Process -Confirm:$false
 $stats = $TheMoveRequestStatistics
 ProcessStats -stats $stats -name ProcessedStats1
 
+#DownloadTheJSonFile
+
+<#
+Get-Process |
+    Select-Object -First 5 name, pm |
+    Out-PieChart -PieChartTitle "Top 5 Windows processes running" -DisplayToScreen
+
+Get-Service |
+	Group-Object -Property Status -NoElement |
+	Out-PieChart -PieChartTitle "Service Status" -Pie3D -saveImage C:\tmp\PieChart\'Service status.png'
+
+#>
+
+$timeline = Build-TimeTrackerTable -MrsJob $TheMoveRequestStatistics -Aggregation Day
+$timeline | ft -AutoSize
+
+
+<#
+[xml]$DiagnosticInfo = $TheMoveRequestStatistics.DiagnosticInfo
+$OutputForChart = Analyze-DiagnosticInfo -DiagnosticInfo $DiagnosticInfo
+#>
 
 #$MoveRequestStatistics = Import-Clixml C:\Temp\Doinita\dtest2.xml
 
